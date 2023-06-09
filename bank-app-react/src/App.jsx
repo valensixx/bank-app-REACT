b3e1fe61797e1399b843f5cb07a771c88c7fc456
valensixx <valensixx@gmail.com>
2023-06-09 12:18:02 +0300

    basic structure of the app done

--- bank-app-react/src/App.jsx
+++ bank-app-react/src/App.jsx
@@ -1,34 +1,38 @@
 import React from "react";
 import styles from "./style";
 
+import { Navbar, Hero, Stats, Bussiness, Billing, CardDeal, Testemonials, Clients, CTA, Footer } from "./components";
+
+
+
+
 const App = () => {
-  return(
+  return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats 
-          Bussiness 
-          Billing 
-          CardDeal 
-          Testemonials 
-          Clients 
-          CTA 
-          Footer 
+          <Stats />
+          <Bussiness />
+          <Billing />
+          <CardDeal />
+          <Testemonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
-
     </div>
   )
 }
