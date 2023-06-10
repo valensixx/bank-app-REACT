838bd16c7c090554bd4a156fc4bf6d7fbc2e32fd
valensixx <valensixx@gmail.com>
2023-06-10 20:15:55 +0300

    working on navbar

--- bank-app-react/src/components/Navbar.jsx
+++ bank-app-react/src/components/Navbar.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { useState } from "react";
 
-import {close, logo, menu} from '../assets';
-import {navLinks} from '../constants';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
     return (
         <nav className="w-full padding py-6 justify-between items-center navbar">
-            <img src={logo} alt = "bank logo" className="w-[124px] h-[32px]"/>
+            <img src={logo} alt="bank logo" className="w-[124px] h-[32px]" />
             <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-                {navLinks.map((nav,index)=>(
-                    <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white`}>
+                {navLinks.map((nav, index) => (
+                    <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white mr-10`}>
                         <a href={`#${nav.id}`}>
                             {nav.title}
                         </a>
                     </li>
                 ))}
             </ul>
+            <div className="ms:hidden flex flex-1 justify-end items-center">
+                <img
+                    src={toggle ? close : menu}
+                    alt="menu"
+                    className="w-[28px] h-[28px] object-contain"
+                    onClick={() => setToggle((prev) => !prev)}
+                />
+            </div>
         </nav>
     )
 }
