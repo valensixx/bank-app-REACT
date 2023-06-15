e30ab0d2676654886eff550c0a3cf4def0761e0f
valensixx <valensixx@gmail.com>
2023-06-15 17:22:55 +0300

    icons done

--- bank-app-react/src/components/Business.jsx
+++ bank-app-react/src/components/Business.jsx
@@ -3,9 +3,11 @@ import { features } from '../constants';
 import styles, { layout } from '../style';
 import Button from './Button';
 
-const FeatureCard = () => (
-    <div>
-        
+const FeatureCard = ({icon, title, content, index }) => (
+    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6": "mb-0"} feature-card`}>
+        <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+            <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain"/>
+        </div>
     </div>
 )
 
@@ -25,7 +27,7 @@ const Business = () => {
             </div>
             <div className={`${layout.sectionImg} flex-col`}>
                 {features.map((feature, index) => (
-                    <FeatureCard />
+                    <FeatureCard key = {feature.id}{...feature} index={index} />
                 ))}
             </div>
         </section>
