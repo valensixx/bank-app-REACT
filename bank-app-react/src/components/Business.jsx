f396a7530c12cd8efb88bc7e4992c5f9989af3a8
valensixx <valensixx@gmail.com>
2023-06-15 16:46:03 +0300

    add h2 and p tags

--- bank-app-react/src/components/Business.jsx
+++ bank-app-react/src/components/Business.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import {features} from '../constants';
-import styles, {layout} from '../style';
+import { features } from '../constants';
+import styles, { layout } from '../style';
 import Button from './Button';
 
 const Business = () => {
     return (
-        <div>Business</div>
+        <section id="fetures" className={layout.section}>
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>
+                    You do the business, <br className="sm:block hidden" /> we’ll handle the money.
+                </h2>
+                <p className={`${styles.paragraph}`}>
+                    With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.
+                </p>
+            </div>
+        </section>
     )
 }
 
