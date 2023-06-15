37224b5ef9fd194d348b62c8c07e9a93f0f56883
valensixx <valensixx@gmail.com>
2023-06-15 16:55:20 +0300

    create button component & take props from business

--- bank-app-react/src/components/Business.jsx
+++ bank-app-react/src/components/Business.jsx
@@ -10,9 +10,10 @@ const Business = () => {
                 <h2 className={styles.heading2}>
                     You do the business, <br className="sm:block hidden" /> we’ll handle the money.
                 </h2>
-                <p className={`${styles.paragraph}`}>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
                     With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.
                 </p>
+                <Button styles= "mt-10"/>
             </div>
         </section>
     )
