4132b6274099f0f4f505eaf61282427b9fca9e92
valensixx <valensixx@gmail.com>
2023-06-15 12:21:48 +0300

    h1 and p tags ready

--- bank-app-react/src/components/Hero.jsx
+++ bank-app-react/src/components/Hero.jsx
@@ -19,13 +19,18 @@ const Hero = () => {
                     <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
                         The Next <br className="sm:block hidden" /> {" "}
                         <span className="text-gradient"> Generation </span> {" "}
-                        Payment Method.
                     </h1>
                     <div className="ss:flex hidden md:mr-4 mr-0">
                         <GetSterted />
                     </div>
-
                 </div>
+                <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+                    Payment Method.
+                </h1>
+                <p className={`${styles.paragraph} max-w-[740px] mt-5`}>
+                    Our team of experts uses a methodology to identify the credit cards most likely to fit your needs.
+                    We examine annual percentage rates, annual fees.
+                </p>
             </div>
         </section>
     )
