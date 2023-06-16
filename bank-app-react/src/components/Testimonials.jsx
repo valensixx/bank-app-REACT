5a414f12f3eea6cb52217d24c4e2f844b6a625dc
valensixx <valensixx@gmail.com>
2023-06-16 14:17:35 +0300

    working on Testimonials.jsx

--- bank-app-react/src/components/Testimonials.jsx
+++ bank-app-react/src/components/Testimonials.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import {feedback} from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
 
 const Testimonials = () => {
     return (
-        <div>Testimonials</div>
+        <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+            <div />
+            <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+                <h1>
+                    What people are <br />
+                    saying about us
+                </h1>
+            </div>
+        </section>
     )
 }
 
