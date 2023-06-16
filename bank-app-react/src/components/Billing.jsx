3360ce1a7eaf9965e06659df94d6e7520288521e
valensixx <valensixx@gmail.com>
2023-06-16 12:52:48 +0300

    work on billing component

--- bank-app-react/src/components/Billing.jsx
+++ bank-app-react/src/components/Billing.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import {apple,bill,google} from '../assets';
+import styles, {layout} from '../style';
 
 const Billing = () => {
     return (
-        <div>Billing</div>
+        <section id="product" className={layout.sectionReverse}>
+            <div className={layout.sectionImgReverse}>
+                <img src={bill} alt="bill img" className="w-[100%] h-[100%] relative z-5" />
+            </div>
+        </section>
     )
 }
 
