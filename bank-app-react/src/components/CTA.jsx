836799ba3774391be4b6d26b5fa90218be6d3c22
valensixx <valensixx@gmail.com>
2023-06-17 16:50:48 +0300

    working on CTA.jsx component

--- bank-app-react/src/components/CTA.jsx
+++ bank-app-react/src/components/CTA.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import styles from "../style";
+import Button from "./Button";
 
 const CTA = () => {
     return (
-        <div>CTA</div>
+        <section>
+            <div>
+                <h2>
+                    Let's try our service now!
+                </h2>
+                <p>
+                    Everything you need to accept 
+                    card payments and grow your business 
+                    anywhere on the planet.
+                </p>
+            </div>
+        </section>
     )
 }
 
