575fe2490024d0f5c55a75f7fa56fa8233b999aa
valensixx <valensixx@gmail.com>
2023-06-17 17:11:20 +0300

    logo img and <p>

--- bank-app-react/src/components/Footer.jsx
+++ bank-app-react/src/components/Footer.jsx
@@ -7,8 +7,14 @@ const Footer = () => {
     return (
         <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
             <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+                <div className="flex-1 flex flex-col justify-start mr-10">
+                    <img src={logo} alt="logo" className="w-[266px] h-[72px] object-contain"/>
+                    <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
+                        A new way to make the payments easy, reliable and secure.
+                    </p>
+                </div>
                 <div>
-                    <img />
+                    
                 </div>
             </div>
         </section>
